f369a08d970fc2597775f2f6c68c890445d6eef4
Peter Jansen Noble <mpjcnoble@tip.edu.ph>
2022-06-07 02:39:31 +0800

    first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '#vw(03o=(9kbvg!&2d5i!2$_58x@_-3l4wujpow6(ym37jxnza'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django4heroku.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['django4heroku21.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
